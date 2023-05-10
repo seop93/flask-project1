73266e852f1e3e4ffa37fdf2063936559f3921ff
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-10 16:40:12 +0900

    Feat: 상품 이미지 API 구현

--- controllers/product.py
+++ controllers/product.py
@@ -1,7 +1,13 @@
+import os
+from datetime import datetime
+
 from flask import request
+from werkzeug.utils import secure_filename
+
 from .blueprint import product
 # from models.mongo import conn_mongodb
 from models.product import Product
+
 
 #상품 API
 @product.route('/regist', methods=['POST'])
@@ -10,12 +16,24 @@ def regist():
     form_data = request.form
 
     #이미지 정보를 저장
-
-
+    thumbnail_img = request.files.get('thumbnail_img')
+    detail_img = request.files.get('detail_img')
+    thumbnail_img_url = _upload_file(thumbnail_img)
+    detail_img_url = _upload_file(detail_img)
     # 저장하는 일
-    Product.insert_one(form_data)
+    Product.insert_one(form_data, thumbnail_img_url, detail_img_url)
 
     return "상품 등록 API입니다."
+
+def _upload_file(img_file):
+    timestamp = str(datetime.now().timestamp)
+    filename = timestamp + '_' + secure_filename(img_file.filename)
+    image_path = f'./static/uploads'
+    os.makedirs(image_path, exist_ok=True)
+    img = os.path.join(image_path, filename)
+    img_file.save(img)
+
+    return f'/static/uploads/' + filename
 
 @product.route('/test', methods=['GET'])
 def test():
