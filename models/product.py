877527d3fcacf298ff775da7957c0716edec7dc2
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-10 16:39:57 +0900

    Feat: 상품 이미지 API 구현

--- models/product.py
+++ models/product.py
@@ -1,11 +1,17 @@
 from .mongo import conn_mongodb
+from datetime import datetime
 
 class Product():
     @staticmethod
-    def insert_one(product):
+    def insert_one(product, thumbnail_img_url, detail_img_url):
         db = conn_mongodb()
         db.products.insert_one({
-            'name': Product['name'],
-            'price': Product['price'],
-            'description': Product['description']
+            'name': product['name'],
+            'price': product['price'],
+            'description': product['description'],
+            'thumbnail_img': thumbnail_img_url,
+            'detail_img': detail_img_url,
+            'user': 'admin',
+            'create_at': int(datetime.now().timestamp()),
+            'update_at': int(datetime.now().timestamp())
         })
