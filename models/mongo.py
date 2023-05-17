8ebc284dc24bc9457dd7451b6dbeffdb65da5a83
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-17 22:57:41 +0900

    Update: ec2서버 주소 수정

--- models/mongo.py
+++ models/mongo.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 mongo_username = os.environ.get('MONGO_USERNAME')
 mongo_password = os.environ.get('MONGO_PASSWORD')
 
-client = MongoClient('ec2-3-38-143-222.ap-northeast-2.compute.amazonaws.com', 27017, username=mongo_username, password=mongo_password)
+client = MongoClient('ec2-15-165-48-208.ap-northeast-2.compute.amazonaws.com', 27017, username=mongo_username, password=mongo_password)
 
 def conn_mongodb():
     db = client.online_store
