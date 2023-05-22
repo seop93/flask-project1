bc74b273ecc52647b675c247c9df5948b8a267b5
seop93 <110223383+seop93@users.noreply.github.com>
2023-05-22 16:26:03 +0900

    Feat: Blueprint users 주소 추가

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 
 from controllers.product import product
-from controllers.user import  user
+from controllers.user import user
 
 
 app = Flask(__name__)
 
+app.secret_key = 'secret'
 
 app.register_blueprint(product, url_prefix='/products')
 app.register_blueprint(user, url_prefix='/users')
